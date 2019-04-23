eaa78939d3ad2d1e67682f13686367b8c1207543
Faizan Muhammad <faizan.muhammad@tufts.edu>
2019-04-22 22:54:51 -0400

    Binned Features

--- main.m
+++ main.m
@@ -1,6 +1,9 @@
 [t, ir] = GetRIR();
-plot(t,ir);
-[da, db, rt] = getDecayFit(t, ir);
+%[da, db, rt] = getDecayFit(t, ir);
+
+[t_bins, d_bins] = getTimeBins(t, ir);
+figure;
+plot(d_bins);
 
 function [a, b, r] = getDecayFit(t, impulse_response)
     DECAYFIT_MEDIANS = 1000;
@@ -21,4 +24,32 @@ function [a, b, r] = getDecayFit(t, impulse_response)
     b = v(2);
     
     r = t(tail_start) - t(max_i);
+end
+
+function [t_bins, d_bins] = getTimeBins(t, impulse_response)
+    NUM_BINS = 20;
+    
+    t_bins = zeros(1, NUM_BINS);
+    d_bins = zeros(1, NUM_BINS);
+    clean = abs(impulse_response);
+    total = sum(clean);
+    section = total/NUM_BINS;
+    target = section;
+    
+    sec_length = floor(length(clean)/NUM_BINS);
+    
+    for i = 1:NUM_BINS
+        curr_sum = 0;
+        curr_index = 1;
+        
+        while curr_sum < target && curr_index < length(impulse_response)
+            curr_sum = curr_sum + clean(curr_index);
+            curr_index = curr_index + 1;
+        end
+        
+        t_bins(i) = t(curr_index);
+        
+        d_bins(i) = mean(clean((i-1)*sec_length + 1:i*sec_length));
+        target = target + section;
+    end
 end

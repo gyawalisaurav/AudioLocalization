0f0e7c2e714a20d104aa7e5522998f1d9345657c
Faizan Muhammad <faizan.muhammad@tufts.edu>
2019-05-04 21:41:57 -0400

    Added Corridor Samples

--- DataCollection.m
+++ DataCollection.m
@@ -1,14 +1,20 @@
-NUM_SAMPLES = 50;
+NUM_POINTS = 25;
+NUM_REPS = 2;
+
 room_name = input('Enter Room Name:', 's');
 room_name = [room_name '.mat'];
 room_samples = [];
 
-for i = 1:NUM_SAMPLES
+for i = 1:NUM_POINTS
     data = [];
     data.pose_x = input('Enter Pose x coordinate:', 's');
     data.pose_y = input('Enter Pose y coordinate:', 's');
-    [data.t, data.ir, data.z] = GetRIR();
-    room_samples = [room_samples, data];
+    
+    for j = 1:NUM_REPS
+        [data.t, data.ir, data.z] = GetRIR();
+        room_samples = [room_samples, data];
+    end
+    
     disp(i);
 end
 
